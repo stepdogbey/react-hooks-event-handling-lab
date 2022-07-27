0daacf055f09f4009edb5b9de8e691135bc807f6
stephen <stevedogbey@gmail.com>
2022-07-27 11:15:38 +0000

    lab done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,26 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  // let onFocus = true;
+  // function handleOnFocus() {
+  //   if ((onFocus = !onFocus)) {
+  //     console.log("Good!");
+  //   } else {
+  //     console.log("Hey! Eyes on me!");
+  //   }
+  // }
+  const onFocus = () => {
+    console.log("Good!");
+  };
+  const onBlur = () => {
+    console.log("Hey! Eyes on me!");
+  };
+  return (
+    <button onFocus={onFocus} onBlur={onBlur}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,17 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  function somethingChange() {
+    console.log("Entering password...");
+  }
+  return (
+    <input
+      onChange={somethingChange}
+      type="password"
+      placeholder="Enter your password"
+    ></input>
+  );
 }
 
 export default Keypad;
